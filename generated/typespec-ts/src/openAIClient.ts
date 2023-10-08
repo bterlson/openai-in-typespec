ff4a23377a28a6c544a0d3cab796c129e4c92100
Mary Gao <yanmeigao1210@gmail.com>
2023-10-08 15:23:21 +0800

    Generate RLC with small emitter fixes

---
+++ generated/typespec-ts/src/openAIClient.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { getClient, ClientOptions } from "@azure-rest/core-client";
+import { logger } from "./logger";
+import { KeyCredential } from "@azure/core-auth";
+import { OpenAIClient } from "./clientDefinitions";
+
+/**
+ * Initialize a new instance of `OpenAIClient`
+ * @param credentials - uniquely identify client credential
+ * @param options - the parameter for all optional parameters
+ */
+export default function createClient(
+  credentials: KeyCredential,
+  options: ClientOptions = {}
+): OpenAIClient {
+  const baseUrl = options.baseUrl ?? `https://api.openai.com/v1`;
+  options.apiVersion = options.apiVersion ?? "2.0.0";
+  const userAgentInfo = `azsdk-js-openai-rest/1.0.0-beta.1`;
+  const userAgentPrefix =
+    options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+      ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
+      : `${userAgentInfo}`;
+  options = {
+    ...options,
+    userAgentOptions: {
+      userAgentPrefix,
+    },
+    loggingOptions: {
+      logger: options.loggingOptions?.logger ?? logger.info,
+    },
+  };
+
+  const client = getClient(baseUrl, options) as OpenAIClient;
+
+  client.pipeline.addPolicy({
+    name: "customKeyCredentialPolicy",
+    async sendRequest(request, next) {
+      request.headers.set("Authorization", "bearer " + credentials.key);
+      return next(request);
+    },
+  });
+  return client;
+}
--- readme.md
+++ readme.md
@@ -1,15 +1,3 @@
-A conversion of the OpenAI OpenAPI to TypeSpec.
+# OpenAPI spec for the OpenAI API
 
-There are some deltas:
-
-### Changes to API Semantics:
-
-- Many things are missing defaults (mostly due to bug where we can't set null defaults)
-- Error responses have been added.
-- Where known, the `object` property's type is narrowed from string to the constant value it will always be
-
-### Changes to API metadata or OpenAPI format
-
-- Much of the x-oaiMeta entries have not been added.
-- In some cases, new schemas needed to be defined in order to be defined in TypeSpec (e.g. because the constraints could not be added to a model property with a heterogeneous type)
-- There is presently no way to set `title`
+This repository contains an [OpenAPI](https://www.openapis.org/) specification for the [OpenAI API](https://platform.openai.com/docs/api-reference).

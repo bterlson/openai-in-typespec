6d6ea7868c4537f550b33278f6e14c8595810999
Patrick Hallisey <hallipr@gmail.com>
2024-03-15 16:12:03 -0700

    Add unit tests with secrets (#23)

---
+++ .dotnet/tests/TestScenarios/GitHubTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace OpenAI.Tests;
+
+public partial class GitHubTests
+{
+    [Test(Description = "Test that we can use a GitHub secret")]
+    [Category("Online")]
+    public void CanUseGitHubSecret()
+    {
+        string gitHubSecretString = Environment.GetEnvironmentVariable("SECRET_VALUE");
+        Assert.That(gitHubSecretString, Is.Not.Null.Or.Empty);
+    }
+
+    [Test(Description = "That that we can run some tests without secrets")]
+    [Category("Offline")]
+    public void CanTestWithoutSecretAccess()
+    {
+        int result = 2 + 1;
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
